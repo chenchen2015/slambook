00dcaf7517b2722c3c9c969afa1aedad0e8c596a
Chen Chen <chenchen.bme@gmail.com>
2019-01-27 20:56:49 +0800

    sync local changes

--- ch7/pose_estimation_2d2d.cpp
+++ ch7/pose_estimation_2d2d.cpp
@@ -1,173 +1,158 @@
 #include <iostream>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-// #include "extra.h" // use this if in OpenCV2 
+// #include "extra.h" // use this if in OpenCV2
 using namespace std;
 using namespace cv;
 
 /****************************************************
- * æœ¬ç¨‹åºæ¼”ç¤ºäº†å¦‚ä½•ä½¿ç”¨2D-2Dçš„ç‰¹å¾åŒ¹é…ä¼°è®¡ç›¸æœºè¿åŠ¨
+ * ±¾³ÌĞòÑİÊ¾ÁËÈçºÎÊ¹ÓÃ2D-2DµÄÌØÕ÷Æ¥Åä¹À¼ÆÏà»úÔË¶¯
  * **************************************************/
 
-void find_feature_matches (
-    const Mat& img_1, const Mat& img_2,
-    std::vector<KeyPoint>& keypoints_1,
-    std::vector<KeyPoint>& keypoints_2,
-    std::vector< DMatch >& matches );
+void find_feature_matches(const Mat& img_1, const Mat& img_2,
+                          std::vector<KeyPoint>& keypoints_1,
+                          std::vector<KeyPoint>& keypoints_2,
+                          std::vector<DMatch>& matches);
 
-void pose_estimation_2d2d (
-    std::vector<KeyPoint> keypoints_1,
-    std::vector<KeyPoint> keypoints_2,
-    std::vector< DMatch > matches,
-    Mat& R, Mat& t );
+void pose_estimation_2d2d(std::vector<KeyPoint> keypoints_1,
+                          std::vector<KeyPoint> keypoints_2,
+                          std::vector<DMatch> matches, Mat& R, Mat& t);
 
-// åƒç´ åæ ‡è½¬ç›¸æœºå½’ä¸€åŒ–åæ ‡
-Point2d pixel2cam ( const Point2d& p, const Mat& K );
+// ÏñËØ×ø±ê×ªÏà»ú¹éÒ»»¯×ø±ê
+Point2d pixel2cam(const Point2d& p, const Mat& K);
 
-int main ( int argc, char** argv )
-{
-    if ( argc != 3 )
-    {
-        cout<<"usage: pose_estimation_2d2d img1 img2"<<endl;
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        cout << "usage: pose_estimation_2d2d img1 img2" << endl;
         return 1;
     }
-    //-- è¯»å–å›¾åƒ
-    Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
-    Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
+    //-- ¶ÁÈ¡Í¼Ïñ
+    Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
 
     vector<KeyPoint> keypoints_1, keypoints_2;
     vector<DMatch> matches;
-    find_feature_matches ( img_1, img_2, keypoints_1, keypoints_2, matches );
-    cout<<"ä¸€å…±æ‰¾åˆ°äº†"<<matches.size() <<"ç»„åŒ¹é…ç‚¹"<<endl;
+    find_feature_matches(img_1, img_2, keypoints_1, keypoints_2, matches);
+    cout << "Ò»¹²ÕÒµ½ÁË" << matches.size() << "×éÆ¥Åäµã" << endl;
 
-    //-- ä¼°è®¡ä¸¤å¼ å›¾åƒé—´è¿åŠ¨
-    Mat R,t;
-    pose_estimation_2d2d ( keypoints_1, keypoints_2, matches, R, t );
+    //-- ¹À¼ÆÁ½ÕÅÍ¼Ïñ¼äÔË¶¯
+    Mat R, t;
+    pose_estimation_2d2d(keypoints_1, keypoints_2, matches, R, t);
 
-    //-- éªŒè¯E=t^R*scale
-    Mat t_x = ( Mat_<double> ( 3,3 ) <<
-                0,                      -t.at<double> ( 2,0 ),     t.at<double> ( 1,0 ),
-                t.at<double> ( 2,0 ),      0,                      -t.at<double> ( 0,0 ),
-                -t.at<double> ( 1,0 ),     t.at<double> ( 0,0 ),      0 );
+    //-- ÑéÖ¤E=t^R*scale
+    Mat t_x = (Mat_<double>(3, 3) << 0, -t.at<double>(2, 0), t.at<double>(1, 0),
+               t.at<double>(2, 0), 0, -t.at<double>(0, 0), -t.at<double>(1, 0),
+               t.at<double>(0, 0), 0);
 
-    cout<<"t^R="<<endl<<t_x*R<<endl;
+    cout << "t^R=" << endl << t_x * R << endl;
 
-    //-- éªŒè¯å¯¹æçº¦æŸ
-    Mat K = ( Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
-    for ( DMatch m: matches )
-    {
-        Point2d pt1 = pixel2cam ( keypoints_1[ m.queryIdx ].pt, K );
-        Mat y1 = ( Mat_<double> ( 3,1 ) << pt1.x, pt1.y, 1 );
-        Point2d pt2 = pixel2cam ( keypoints_2[ m.trainIdx ].pt, K );
-        Mat y2 = ( Mat_<double> ( 3,1 ) << pt2.x, pt2.y, 1 );
+    //-- ÑéÖ¤¶Ô¼«Ô¼Êø
+    Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
+    for (DMatch m : matches) {
+        Point2d pt1 = pixel2cam(keypoints_1[m.queryIdx].pt, K);
+        Mat y1 = (Mat_<double>(3, 1) << pt1.x, pt1.y, 1);
+        Point2d pt2 = pixel2cam(keypoints_2[m.trainIdx].pt, K);
+        Mat y2 = (Mat_<double>(3, 1) << pt2.x, pt2.y, 1);
         Mat d = y2.t() * t_x * R * y1;
         cout << "epipolar constraint = " << d << endl;
     }
+    
     return 0;
 }
 
-void find_feature_matches ( const Mat& img_1, const Mat& img_2,
-                            std::vector<KeyPoint>& keypoints_1,
-                            std::vector<KeyPoint>& keypoints_2,
-                            std::vector< DMatch >& matches )
-{
-    //-- åˆå§‹åŒ–
+void find_feature_matches(const Mat& img_1, const Mat& img_2,
+                          std::vector<KeyPoint>& keypoints_1,
+                          std::vector<KeyPoint>& keypoints_2,
+                          std::vector<DMatch>& matches) {
+    //-- ³õÊ¼»¯
     Mat descriptors_1, descriptors_2;
-    // used in OpenCV3 
+    // used in OpenCV3
     Ptr<FeatureDetector> detector = ORB::create();
     Ptr<DescriptorExtractor> descriptor = ORB::create();
-    // use this if you are in OpenCV2 
+    // use this if you are in OpenCV2
     // Ptr<FeatureDetector> detector = FeatureDetector::create ( "ORB" );
-    // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB" );
-    Ptr<DescriptorMatcher> matcher  = DescriptorMatcher::create ( "BruteForce-Hamming" );
-    //-- ç¬¬ä¸€æ­¥:æ£€æµ‹ Oriented FAST è§’ç‚¹ä½ç½®
-    detector->detect ( img_1,keypoints_1 );
-    detector->detect ( img_2,keypoints_2 );
+    // Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ( "ORB"
+    // );
+    Ptr<DescriptorMatcher> matcher =
+        DescriptorMatcher::create("BruteForce-Hamming");
+    //-- µÚÒ»²½:¼ì²â Oriented FAST ½ÇµãÎ»ÖÃ
+    detector->detect(img_1, keypoints_1);
+    detector->detect(img_2, keypoints_2);
 
-    //-- ç¬¬äºŒæ­¥:æ ¹æ®è§’ç‚¹ä½ç½®è®¡ç®— BRIEF æè¿°å­
-    descriptor->compute ( img_1, keypoints_1, descriptors_1 );
-    descriptor->compute ( img_2, keypoints_2, descriptors_2 );
+    //-- µÚ¶ş²½:¸ù¾İ½ÇµãÎ»ÖÃ¼ÆËã BRIEF ÃèÊö×Ó
+    descriptor->compute(img_1, keypoints_1, descriptors_1);
+    descriptor->compute(img_2, keypoints_2, descriptors_2);
 
-    //-- ç¬¬ä¸‰æ­¥:å¯¹ä¸¤å¹…å›¾åƒä¸­çš„BRIEFæè¿°å­è¿›è¡ŒåŒ¹é…ï¼Œä½¿ç”¨ Hamming è·ç¦»
+    //-- µÚÈı²½:¶ÔÁ½·ùÍ¼ÏñÖĞµÄBRIEFÃèÊö×Ó½øĞĞÆ¥Åä£¬Ê¹ÓÃ Hamming ¾àÀë
     vector<DMatch> match;
-    //BFMatcher matcher ( NORM_HAMMING );
-    matcher->match ( descriptors_1, descriptors_2, match );
+    // BFMatcher matcher ( NORM_HAMMING );
+    matcher->match(descriptors_1, descriptors_2, match);
 
-    //-- ç¬¬å››æ­¥:åŒ¹é…ç‚¹å¯¹ç­›é€‰
-    double min_dist=10000, max_dist=0;
+    //-- µÚËÄ²½:Æ¥Åäµã¶ÔÉ¸Ñ¡
+    double min_dist = 10000, max_dist = 0;
 
-    //æ‰¾å‡ºæ‰€æœ‰åŒ¹é…ä¹‹é—´çš„æœ€å°è·ç¦»å’Œæœ€å¤§è·ç¦», å³æ˜¯æœ€ç›¸ä¼¼çš„å’Œæœ€ä¸ç›¸ä¼¼çš„ä¸¤ç»„ç‚¹ä¹‹é—´çš„è·ç¦»
-    for ( int i = 0; i < descriptors_1.rows; i++ )
-    {
+    //ÕÒ³öËùÓĞÆ¥ÅäÖ®¼äµÄ×îĞ¡¾àÀëºÍ×î´ó¾àÀë,
+    //¼´ÊÇ×îÏàËÆµÄºÍ×î²»ÏàËÆµÄÁ½×éµãÖ®¼äµÄ¾àÀë
+    for (int i = 0; i < descriptors_1.rows; i++) {
         double dist = match[i].distance;
-        if ( dist < min_dist ) min_dist = dist;
-        if ( dist > max_dist ) max_dist = dist;
+        if (dist < min_dist) min_dist = dist;
+        if (dist > max_dist) max_dist = dist;
     }
 
-    printf ( "-- Max dist : %f \n", max_dist );
-    printf ( "-- Min dist : %f \n", min_dist );
+    printf("-- Max dist : %f \n", max_dist);
+    printf("-- Min dist : %f \n", min_dist);
 
-    //å½“æè¿°å­ä¹‹é—´çš„è·ç¦»å¤§äºä¸¤å€çš„æœ€å°è·ç¦»æ—¶,å³è®¤ä¸ºåŒ¹é…æœ‰è¯¯.ä½†æœ‰æ—¶å€™æœ€å°è·ç¦»ä¼šéå¸¸å°,è®¾ç½®ä¸€ä¸ªç»éªŒå€¼30ä½œä¸ºä¸‹é™.
-    for ( int i = 0; i < descriptors_1.rows; i++ )
-    {
-        if ( match[i].distance <= max ( 2*min_dist, 30.0 ) )
-        {
-            matches.push_back ( match[i] );
+    //µ±ÃèÊö×ÓÖ®¼äµÄ¾àÀë´óÓÚÁ½±¶µÄ×îĞ¡¾àÀëÊ±,¼´ÈÏÎªÆ¥ÅäÓĞÎó.µ«ÓĞÊ±ºò×îĞ¡¾àÀë»á·Ç³£Ğ¡,ÉèÖÃÒ»¸ö¾­ÑéÖµ30×÷ÎªÏÂÏŞ.
+    for (int i = 0; i < descriptors_1.rows; i++) {
+        if (match[i].distance <= max(2 * min_dist, 30.0)) {
+            matches.push_back(match[i]);
         }
     }
 }
 
-
-Point2d pixel2cam ( const Point2d& p, const Mat& K )
-{
-    return Point2d
-           (
-               ( p.x - K.at<double> ( 0,2 ) ) / K.at<double> ( 0,0 ),
-               ( p.y - K.at<double> ( 1,2 ) ) / K.at<double> ( 1,1 )
-           );
+Point2d pixel2cam(const Point2d& p, const Mat& K) {
+    return Point2d((p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
+                   (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1));
 }
 
+void pose_estimation_2d2d(std::vector<KeyPoint> keypoints_1,
+                          std::vector<KeyPoint> keypoints_2,
+                          std::vector<DMatch> matches, Mat& R, Mat& t) {
+    // Ïà»úÄÚ²Î,TUM Freiburg2
+    Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
 
-void pose_estimation_2d2d ( std::vector<KeyPoint> keypoints_1,
-                            std::vector<KeyPoint> keypoints_2,
-                            std::vector< DMatch > matches,
-                            Mat& R, Mat& t )
-{
-    // ç›¸æœºå†…å‚,TUM Freiburg2
-    Mat K = ( Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
-
-    //-- æŠŠåŒ¹é…ç‚¹è½¬æ¢ä¸ºvector<Point2f>çš„å½¢å¼
+    //-- °ÑÆ¥Åäµã×ª»»Îªvector<Point2f>µÄĞÎÊ½
     vector<Point2f> points1;
     vector<Point2f> points2;
 
-    for ( int i = 0; i < ( int ) matches.size(); i++ )
-    {
-        points1.push_back ( keypoints_1[matches[i].queryIdx].pt );
-        points2.push_back ( keypoints_2[matches[i].trainIdx].pt );
+    for (int i = 0; i < (int)matches.size(); i++) {
+        points1.push_back(keypoints_1[matches[i].queryIdx].pt);
+        points2.push_back(keypoints_2[matches[i].trainIdx].pt);
     }
 
-    //-- è®¡ç®—åŸºç¡€çŸ©é˜µ
+    //-- ¼ÆËã»ù´¡¾ØÕó
     Mat fundamental_matrix;
-    fundamental_matrix = findFundamentalMat ( points1, points2, CV_FM_8POINT );
-    cout<<"fundamental_matrix is "<<endl<< fundamental_matrix<<endl;
+    fundamental_matrix = findFundamentalMat(points1, points2, CV_FM_8POINT);
+    cout << "fundamental_matrix is " << endl << fundamental_matrix << endl;
 
-    //-- è®¡ç®—æœ¬è´¨çŸ©é˜µ
-    Point2d principal_point ( 325.1, 249.7 );	//ç›¸æœºå…‰å¿ƒ, TUM datasetæ ‡å®šå€¼
-    double focal_length = 521;			//ç›¸æœºç„¦è·, TUM datasetæ ‡å®šå€¼
+    //-- ¼ÆËã±¾ÖÊ¾ØÕó
+    Point2d principal_point(325.1, 249.7);  //Ïà»ú¹âĞÄ, TUM dataset±ê¶¨Öµ
+    double focal_length = 521;              //Ïà»ú½¹¾à, TUM dataset±ê¶¨Öµ
     Mat essential_matrix;
-    essential_matrix = findEssentialMat ( points1, points2, focal_length, principal_point );
-    cout<<"essential_matrix is "<<endl<< essential_matrix<<endl;
+    essential_matrix =
+        findEssentialMat(points1, points2, focal_length, principal_point);
+    cout << "essential_matrix is " << endl << essential_matrix << endl;
 
-    //-- è®¡ç®—å•åº”çŸ©é˜µ
+    //-- ¼ÆËãµ¥Ó¦¾ØÕó
     Mat homography_matrix;
-    homography_matrix = findHomography ( points1, points2, RANSAC, 3 );
-    cout<<"homography_matrix is "<<endl<<homography_matrix<<endl;
+    homography_matrix = findHomography(points1, points2, RANSAC, 3);
+    cout << "homography_matrix is " << endl << homography_matrix << endl;
 
-    //-- ä»æœ¬è´¨çŸ©é˜µä¸­æ¢å¤æ—‹è½¬å’Œå¹³ç§»ä¿¡æ¯.
-    recoverPose ( essential_matrix, points1, points2, R, t, focal_length, principal_point );
-    cout<<"R is "<<endl<<R<<endl;
-    cout<<"t is "<<endl<<t<<endl;
-    
+    //-- ´Ó±¾ÖÊ¾ØÕóÖĞ»Ö¸´Ğı×ªºÍÆ½ÒÆĞÅÏ¢.
+    recoverPose(essential_matrix, points1, points2, R, t, focal_length,
+                principal_point);
+    cout << "R is " << endl << R << endl;
+    cout << "t is " << endl << t << endl;
 }
